82cc8a4b98f1fc1af797e1fbc7989a95ff93d838
Pieter Van Der Helst <pieter@heyheyapps.com>
2019-01-29 17:33:13 +0100

    framework for slides

---
+++ plugin/codestepper/codestepper.js
@@ -0,0 +1,260 @@
+const CodeStepper =
+  window.CodeStepper ||
+  (function() {
+    function toArray(o) {
+      return Array.prototype.slice.call(o);
+    }
+
+    class Range {
+      // will parse a string of the form "1,2,3-7,9-12,14"
+      // and create an object with a 'range' key with [1,2,3,4,5,6,7,9,10,11,12,14] as an array of integers
+      // and a 'forever' key, either true or false, denoting if all indices greater than the last are included
+      // (constructed by +, e.g. "1-4,5,8+")
+      constructor(stringRange) {
+        stringRange = stringRange.trim();
+        this.range = [];
+        // if the last char is a +, the range includes all indexes greater than the last one parsed
+        this.forever = false;
+        if (stringRange.slice(-1) === '+') {
+          this.forever = true;
+          stringRange = stringRange.slice(0, -1);
+        }
+        if (stringRange.length) {
+          stringRange.split(',').forEach(el => {
+            let dashRange = el.split('-');
+            if (dashRange.length === 1) {
+              this.range.push(parseInt(el));
+            } else {
+              let from = parseInt(dashRange[0]);
+              let to = parseInt(dashRange[1]);
+              if (to >= from) {
+                for (let i = from; i <= to; ++i) {
+                  this.range.push(i);
+                }
+              } else {
+                console.error(`range a-b with a > b "${from}-${to}"`);
+              }
+            }
+          });
+        }
+      }
+
+      max() {
+        if (this.range.length === 0) {
+          return 0;
+        }
+        return Math.max(...this.range);
+      }
+
+      firstIndex() {
+        if (this.range.length === 0) {
+          return -1;
+        }
+        return this.range[0];
+      }
+
+      includes(index) {
+        return (
+          this.range.includes(index) || (this.forever && index > this.max())
+        );
+      }
+    }
+
+    // if a <section> contains a <div> with the attribute 'codesteps'
+    // expand the section in many sections, one for each step, with
+    // appropriate span's to highlight / hide stuff in each step
+    class CodeSection {
+      constructor(sectionEl) {
+        this.highlightFirstShown = true;
+        this.codeExplanations = new Map(); // map key is a Range object, value the explanation itself
+        this.maxIndex = 0;
+        [this.header, this.code, this.footer] = this.splitSection(sectionEl);
+        this._convertedSections = null;
+      }
+
+      splitSection(sectionEl) {
+        // there should be a 'codesteps' div here, that's the code part with explanations
+        // everything before: header
+        // everything after: footer
+        let header = [];
+        let code = null;
+        let footer = [];
+        sectionEl.childNodes.forEach(child => {
+          if (child.nodeType === 1 && child.hasAttribute('codesteps')) {
+            // we should have a <pre> part with the code, and a <div explanation> with all the explanations
+            // (and nothing else)
+            if (child.hasAttribute('no-highlight-first')) {
+              this.highlightFirstShown = false;
+            }
+            code = child.querySelector('pre');
+            const explanationDiv = child.querySelector('div[explanation]');
+            let explanationNodes = explanationDiv.querySelectorAll(
+              'span[step]'
+            );
+            explanationNodes.forEach(explanationNode => {
+              const explanationRange = new Range(
+                explanationNode.getAttribute('step')
+              );
+              let explanation = document.createElement('div');
+              explanation.classList.add('code-explanation');
+              // copy style, so we can override default behaviour in our slides
+              // (e.g. greater width or whatever)
+              if (explanationNode.hasAttribute('style')) {
+                explanation.setAttribute(
+                  'style',
+                  explanationNode.getAttribute('style')
+                );
+              }
+              if (
+                explanationNode.childNodes.length > 1 ||
+                (explanationNode.childNodes[0] &&
+                  explanationNode.childNodes[0].nodeType === 1)
+              ) {
+                // explanation = new DocumentFragment();
+                explanationNode.childNodes.forEach(n =>
+                  explanation.appendChild(n.cloneNode(true))
+                );
+              } else if (explanationNode.childNodes[0]) {
+                explanation.appendChild(
+                  explanationNode.childNodes[0].cloneNode(true)
+                );
+              }
+              this.codeExplanations.set(explanationRange, explanation);
+              this.maxIndex = Math.max(this.maxIndex, explanationRange.max());
+            });
+          } else {
+            if (!code) {
+              header.push(child);
+            } else {
+              footer.push(child);
+            }
+          }
+        });
+        return [header, code, footer];
+      }
+
+      processForIndex(node, idx) {
+        // if (node.nodeType !== 1) {
+        //   return node;
+        // }
+        // console.log(`process for index ${idx}`);
+        let hstepDivs = node.querySelectorAll('span[hstep]');
+        hstepDivs.forEach(hstepDiv => {
+          const highlightRange = new Range(hstepDiv.getAttribute('hstep'));
+          if (highlightRange.includes(idx)) {
+            const clonedDiv = hstepDiv.cloneNode(true);
+            clonedDiv.classList.add('code-highlight');
+            hstepDiv.parentNode.replaceChild(clonedDiv, hstepDiv);
+            // hstepDiv.classList.add("code-highlight");
+          } else {
+            hstepDiv.classList.remove('code-highlight');
+          }
+        });
+        let sstepDivs = node.querySelectorAll('span[sstep]');
+        sstepDivs.forEach(sstepDiv => {
+          const showRange = new Range(sstepDiv.getAttribute('sstep'));
+          if (showRange.firstIndex() == idx) {
+            sstepDiv.removeAttribute('hidden');
+            if (idx != 1 || this.highlightFirstShown) {
+              sstepDiv.classList.add('code-firstshown');
+            }
+          } else if (showRange.includes(idx)) {
+            if (idx != 1 || this.highlightFirstShown) {
+              sstepDiv.classList.remove('code-firstshown');
+            }
+          } else {
+            sstepDiv.setAttribute('hidden', true);
+          }
+        });
+        return node;
+      }
+
+      createExplanationDivForIndex(idx) {
+        let gotExplanation = false;
+        let explDiv = document.createElement('div');
+        explDiv.classList.add('explanations');
+        this.codeExplanations.forEach((explanations, range) => {
+          if (range.includes(idx)) {
+            // const newDiv = document.createElement("div");
+            // newDiv.classList.add("code-explanation");
+            // newDiv.appendChild(explanations.cloneNode(true));
+            explDiv.appendChild(explanations);
+            gotExplanation = true;
+          }
+        });
+        return gotExplanation ? explDiv : null;
+      }
+
+      createSectionForIndex(idx) {
+        let newSection = document.createElement('section');
+        const transition =
+          idx == 1 ? 'slide-in none' : idx == this.maxIndex ? 'none' : 'none'; // "none slide-out" for last slide flickers, something with revealjs...
+        newSection.setAttribute('data-transition', transition);
+        this.header.forEach(el => {
+          newSection.appendChild(el.cloneNode(true));
+        });
+
+        const codeExplDiv = document.createElement('div');
+        codeExplDiv.classList.add('code-with-explanation');
+        let addCodeExplDiv = false;
+
+        if (this.code) {
+          const newNode = this.processForIndex(this.code, idx);
+          codeExplDiv.appendChild(newNode.cloneNode(true));
+          addCodeExplDiv = true;
+        }
+        this.footer.forEach(el => newSection.appendChild(el.cloneNode(true)));
+        let explDiv = this.createExplanationDivForIndex(idx);
+        if (explDiv) {
+          codeExplDiv.appendChild(explDiv);
+          addCodeExplDiv = true;
+        }
+        if (addCodeExplDiv) {
+          newSection.appendChild(codeExplDiv);
+        }
+        return newSection;
+      }
+
+      get convertedSections() {
+        if (!this._convertedSections) {
+          this._convertedSections = new Array();
+          for (let i = 1; i <= this.maxIndex; ++i) {
+            this._convertedSections.push(this.createSectionForIndex(i));
+          }
+        }
+        return this._convertedSections;
+      }
+    }
+
+    function sectionMustExpand(section) {
+      // check if we have a div with the 'codesteps' attribute
+      return toArray(section.getElementsByTagName('div')).filter(el =>
+        el.hasAttribute('codesteps')
+      ).length;
+    }
+
+    return {
+      initialize: function() {
+        let sections = toArray(document.getElementsByTagName('section'));
+        sections.forEach(sec => {
+          if (sectionMustExpand(sec)) {
+            let newSection = new CodeSection(sec);
+            [
+              firstNewSection,
+              ...otherNewSections
+            ] = newSection.convertedSections;
+
+            sec.parentNode.replaceChild(firstNewSection, sec);
+            let lastAdded = firstNewSection;
+            otherNewSections.forEach(otherNew => {
+              firstNewSection.parentNode.insertBefore(
+                otherNew,
+                lastAdded.nextSibling
+              );
+              lastAdded = otherNew;
+            });
+          }
+        });
+      }
+    };
+  })();
---
+++ plugin/codestepper/svgstepper.js
@@ -0,0 +1,394 @@
+var SvgStepper =
+  window.SvgStepper ||
+  (function() {
+    // if I'd ever feel the need, these could become options somehow
+    const highlightFirstAppearanceInCodeBlocks = true;
+
+    let currentIndex = -1;
+    let maxIndex = -1;
+
+    let codeElements = new Set();
+    let svgElements = new Set();
+
+    let snap = null; // for now, only one active svg
+
+    // top is readjusted (default Reveal handling of vertically centering falls short if
+    // show/hide large portions of the slide)
+    let previousTop = -1;
+    let currentSection = undefined;
+
+    function toArray(o) {
+      return Array.prototype.slice.call(o);
+    }
+
+    class Range {
+      // will parse a string of the form "1,2,3-7,9-12,14"
+      // and return an object with a 'range' key with [1,2,3,4,5,6,7,9,10,11,12,14] as an array of integers
+      // and a 'forever' key, either true or false, denoting if all indices greater than the last are included
+      // (constructed by +, e.g. "1-4,5,8+")
+      constructor(stringRange) {
+        stringRange = stringRange.trim();
+        this.range = [];
+        // if the last char is a +, the range includes all indexes greater than the last one parsed
+        this.forever = false;
+        if (stringRange.slice(-1) === "+") {
+          this.forever = true;
+          stringRange = stringRange.slice(0, -1);
+        }
+        if (stringRange.length) {
+          stringRange.split(",").forEach(el => {
+            let dashRange = el.split("-");
+            if (dashRange.length === 1) {
+              this.range.push(parseInt(el));
+            } else {
+              let from = parseInt(dashRange[0]);
+              let to = parseInt(dashRange[1]);
+              if (to >= from) {
+                for (let i = from; i <= to; ++i) {
+                  this.range.push(i);
+                }
+              } else {
+                console.error(`range a-b with a > b "${from}-${to}"`);
+              }
+            }
+          });
+        }
+      }
+
+      max() {
+        if (this.range.length === 0) {
+          return 0;
+        }
+        return Math.max(...this.range);
+      }
+
+      firstIndex() {
+        if (this.range.length === 0) {
+          return -1;
+        }
+        return this.range[0];
+      }
+
+      includes(index) {
+        return (
+          this.range.includes(index) || (this.forever && index > this.max())
+        );
+      }
+    }
+
+    // slides are centered vertically by once setting the 'top' style based
+    // on the scrollHeight / contentHeight, because I dynamically hide/show
+    // elements this scrollHeight changes, but the top is not reapplied,
+    // so I look for it here and set it correctly
+    function readjustTopOfCurrentSection() {
+      while (currentSection && currentSection.tagName != "SECTION") {
+        if (currentSection.parentNode) {
+          currentSection = currentSection.parentNode;
+        } else {
+          currentSection = undefined;
+          break;
+        }
+      }
+      const newTop =
+        (Reveal.getConfig().height - currentSection.scrollHeight) / 2;
+      // only set if more than 30px difference (this is enough to not readjust for
+      // one extra line of text in explanation)
+      if (
+        previousTop < 0 ||
+        newTop - previousTop > 30 ||
+        previousTop - newTop > 30
+      ) {
+        currentSection.style.top = `${newTop}px`;
+        previousTop = newTop;
+      } else {
+        // console.log(`prev ${previousTop}, new ${newTop}`);
+      }
+    }
+
+    // class that handles codelines inside <code> tag, and the explanation underneath
+    class CodeElement {
+      // showRange and highlighRange are objects of the type
+      // {
+      //    range: [1,2,4,5,9,12],
+      //    forever: true  // (meaning everything after 12 too)
+      // }
+      constructor(el, showRange, highlightRange = new Range("")) {
+        this.htmlElement = el;
+        this.showRange = showRange;
+        this.highlightRange = highlightRange;
+
+        // only do 'highlight first in green' inside code blocks (not text underneath)
+        this.insideCodeTag = false;
+        let pnode = el.parentNode;
+        while (pnode && pnode.nodeName != "HTML") {
+          if (pnode.nodeName === "CODE") {
+            this.insideCodeTag = true;
+            break;
+          }
+          pnode = pnode.parentNode;
+        }
+      }
+      update(index) {
+        if (this.showRange.max()) {
+          if (this.showRange.includes(index)) {
+            this.htmlElement.removeAttribute("hidden");
+            // first time something is shown, it's highlighted in green (unless options say otherwhise)
+            if (
+              index != 1 && // don't do this when something is show from the start (so don't do it when slide appears)
+              highlightFirstAppearanceInCodeBlocks &&
+              !this.htmlElement.hasAttribute("no-highlight-first") &&
+              this.insideCodeTag &&
+              this.showRange.firstIndex() === index
+            ) {
+              this.htmlElement.classList.add("highlight-green");
+            } else {
+              this.htmlElement.classList.remove("highlight-green");
+            }
+          } else {
+            this.htmlElement.setAttribute("hidden", true);
+            this.htmlElement.classList.remove("highlight-green");
+          }
+        }
+        if (this.highlightRange.includes(index)) {
+          this.htmlElement.classList.add("highlight-red");
+        } else {
+          this.htmlElement.classList.remove("highlight-red");
+        }
+      }
+    }
+
+    class SvgElement {
+      constructor(el, showRange, highlightRange, highlightColor) {
+        this.svgElement = el;
+        this.showRange = showRange;
+        this.highlightRange = highlightRange;
+        this.highlightColor = highlightColor;
+      }
+      update(index) {
+        if (!this.showRange || this.showRange.includes(index)) {
+          this.svgElement.animate({ opacity: 1.0 }, 250, mina.easeinout);
+
+          if (this.highlightRange) {
+            if (this.highlightRange.includes(index)) {
+              this.svgElement.node.setAttribute("fill", this.highlightColor);
+            } else {
+              this.svgElement.node.removeAttribute("fill");
+            }
+          }
+        } else {
+          this.svgElement.animate({ opacity: 0.0 }, 0);
+        }
+      }
+    }
+
+    function showHighlightCurrentIndex() {
+      if (currentIndex > maxIndex) {
+        // at the end of our inner navigation, remove all keyboard overrides
+        // (and hence, give control back to Reveal)
+        Reveal.configure({
+          keyboard: {},
+          touches: {}
+        });
+        Reveal.navigateNext();
+      } else if (currentIndex <= 0) {
+        Reveal.configure({
+          keyboard: {},
+          touches: {}
+        });
+        Reveal.navigatePrev();
+      } else {
+        codeElements.forEach(el => el.update(currentIndex));
+        svgElements.forEach(el => el.update(currentIndex));
+        readjustTopOfCurrentSection();
+      }
+    }
+
+    // these functions will be bound to all the keys
+    // which are used to advance a slide (down, right, etc.)
+    function innerNavigateNext() {
+      currentIndex++;
+      showHighlightCurrentIndex();
+    }
+
+    function innerNavigatePrevious() {
+      currentIndex--;
+      showHighlightCurrentIndex();
+    }
+
+    // constructs the lists and sets up inner navigation
+    // when a fragment with the correct attribute is encountered
+    Reveal.addEventListener("fragmentshown", function(event) {
+      console.log(event);
+      currentSection = event.fragment; // not correct, but using it the first time will adjust this
+      let needsInnerNavigation = false;
+      currentIndex = 1;
+      maxIndex = -1;
+
+      if (event.fragment.hasAttribute("svg-step")) {
+        console.log("found svg-step");
+        needsInnerNavigation = true;
+        svgElements = new Set();
+        maxIndex = 2; // set it to something > 1, will be overriden by either codestep, or the ASYNC loading of svg
+        // (if there's nothing but a svg, we try to show index 1, while max is still -1 (loading is async),
+        // and we move to the next slide)
+        toArray(event.fragment.getElementsByTagName("svg"))
+          .filter(el => el.classList.contains("svg-section"))
+          .forEach(el => {
+            if (false && snap) {
+              console.error(
+                `I can only cope with one svg element inside a svg-step div (for now)`
+              );
+            }
+            snap = new Snap(`#${el.getAttribute("id")}`);
+
+            // setTimeout(() => {
+            Snap.load(`${el.getAttribute("snapfile")}`, data => {
+              // the read file gets appended, so if we go back / fwd we would end up
+              // with multiple svg's drawn on top of each other, so remove previous one (if any)
+              if (snap.children()) {
+                snap.children().forEach(x => {
+                  x.remove();
+                });
+              }
+              // console.log(data);
+              let defs = data.select("defs");
+              snap.append(defs);
+              let g = data.select("#maincanvas");
+              if (!g) {
+                console.error(
+                  "Codestepper needs a 'g' canvas with the id maincanvas"
+                );
+              }
+              snap.append(g);
+              // set viewBox to the same one it was in the file
+              el.setAttribute("viewBox", snap.getBBox().vb);
+              console.log(`viewBox set to ${snap.getBBox().vb}`);
+
+              let x = g.selectAll(".svgstep");
+              x.forEach(item => {
+                item.node.setAttribute("opacity", 0.0); // start hidden, or the screen 'flashes'
+                let showStepRange = new Range("");
+                if (item.node.hasAttribute("show-steps")) {
+                  showStepRange = new Range(
+                    item.node.getAttribute("show-steps")
+                  );
+                }
+
+                svgElements.add(new SvgElement(item, showStepRange));
+                maxIndex = Math.max(maxIndex, showStepRange.max());
+              });
+
+              let h = g.selectAll(".svghighlight");
+              h.forEach(item => {
+                let highlightRange = new Range("");
+                if (item.node.hasAttribute("highlight-steps")) {
+                  highlightRange = new Range(
+                    item.node.getAttribute("highlight-steps")
+                  );
+                }
+                let highlightColor = "#ff7e79"; // orange
+                if (item.node.hasAttribute("highlight-color")) {
+                  let theColor = item.node.getAttribute("highlight-color");
+                  switch (theColor) {
+                    case "orange":
+                      highlightColor = "#ff7e79";
+                      break;
+                    case "green":
+                      highlightColor = "#929000";
+                      break;
+                    case "blue":
+                      highlightColor = "#0096ff";
+                      break;
+                    case "purple":
+                      highlightColor = "#9437ff";
+                      break;
+                    default:
+                      highlightColor = theColor;
+                  }
+                }
+                svgElements.add(
+                  new SvgElement(item, null, highlightRange, highlightColor)
+                );
+              });
+
+              showHighlightCurrentIndex(); // loading is async, so make sure our svg is loaded according to current index
+            });
+            //}, 2000);
+          });
+      }
+
+      if (event.fragment.hasAttribute("code-step")) {
+        needsInnerNavigation = true;
+        codeElements = new Set();
+
+        // first adapt the 'samespot' childNodes, remove extra added newlines (it's inside a code block)
+        // and add css to the inner span's
+        toArray(event.fragment.getElementsByTagName("*")).forEach(el => {
+          if (el.classList.contains("samespot")) {
+            for (let i = 0; i < el.childNodes.length; ) {
+              if (el.childNodes[i].nodeType == 3) {
+                // TEXT
+                // remove all the newlines that were used to add the different <span> childnodes
+                // in a readable manner (the <pre><code> preserves them, leading to superfluous whitespace)
+                el.removeChild(el.childNodes[i]);
+              } else {
+                if (
+                  el.childNodes[i].nodeName === "SPAN" ||
+                  el.childNodes[i].nodeName === "P"
+                ) {
+                  el.childNodes[i].classList.add("samespot-content");
+                }
+                ++i;
+              }
+            }
+          }
+        });
+        // loop again over the children (above loop changed the DOM), now simply remember all the
+        // show-steps, highlight-steps spans
+        toArray(event.fragment.getElementsByTagName("*")).forEach(el => {
+          let showStepRange = new Range("");
+          if (el.hasAttribute("show-steps")) {
+            showStepRange = new Range(el.getAttribute("show-steps"));
+          }
+          let highlightRange = new Range("");
+          if (el.hasAttribute("highlight-steps")) {
+            highlightRange = new Range(el.getAttribute("highlight-steps"));
+          }
+          if (showStepRange.max() || highlightRange.max()) {
+            maxIndex = Math.max(
+              showStepRange.max(),
+              highlightRange.max(),
+              maxIndex
+            );
+            codeElements.add(
+              new CodeElement(el, showStepRange, highlightRange)
+            );
+          }
+        });
+      }
+
+      if (needsInnerNavigation) {
+        showHighlightCurrentIndex();
+
+        // (ab)use the user defined keyboard shortcuts to 'swallow' ->, space etc.
+        Reveal.configure({
+          keyboard: {
+            34: innerNavigateNext,
+            78: innerNavigateNext,
+            39: innerNavigateNext,
+            76: innerNavigateNext,
+            40: innerNavigateNext,
+            80: innerNavigatePrevious,
+            33: innerNavigatePrevious,
+            72: innerNavigatePrevious,
+            37: innerNavigatePrevious,
+            38: innerNavigatePrevious
+          },
+          touches: {
+            swipeLeft: innerNavigatePrevious,
+            swipeRight: innerNavigateNext
+          }
+        });
+      }
+    });
+  })();
